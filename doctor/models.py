4e558a89af82b85c87fd120e4a99936e4dd92ffa
rotemhadad <rotem6432@gmail.com>
2022-05-06 13:06:16 +0300

    changes in views

--- doctor/models.py
+++ doctor/models.py
@@ -31,7 +31,7 @@ class Patient(models.Model):
     name = models.CharField(max_length=8, null = True, default = None)
     lastName = models.CharField(max_length=8, null = True, default = None)
     patient_id = models.CharField(max_length=9,null = True,default = None)
-    age = models.CharField(max_length=9,null = True,default = None)
+    age = models.CharField(max_length=9,null = True,default = "0")
     gender = models.CharField(max_length=9,null = True,default = None)
     smoke = models.BooleanField(null= True,default = False)
     eastCommunity = models.BooleanField(null= True,default = False)
@@ -39,8 +39,8 @@ class Patient(models.Model):
     ethiopian =  models.BooleanField(null= True,default = False)
     medicine = models.BooleanField(null= True,default = False) #before the meeting
     bloodTest = models.ForeignKey(BloodTest, on_delete = models.CASCADE,null= True,default = None)
-    diagnose = models.CharField(max_length=300,null = True,default = None)
-    treatment = models.CharField(max_length=500,null = True,default = None)
+    diagnose = models.TextField(max_length=1000,null = True,default = None)
+    treatment = models.TextField(max_length=1000,null = True,default = None)
 
 
     def __str__(self):
--- doctor/views.py
+++ doctor/views.py
@@ -20,7 +20,7 @@ import datetime as dt
 import os
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
-
+import simplejson as json
 
 # Create your views here.
 
@@ -239,15 +239,206 @@ def patientQ(request,user_id):
         else:
             bloodTest=BloodTest(WBC=WBC,Neut=Neut,Lymph=Lymph,RBC=RBC,HCT=HCT,Urea=Urea,Hb=Hb,Crtn=Crtn,Iron=Iron,HDL=HDL,AP=AP)
     bloodTest.save()
+    
     patient = Patient(patient_id=patient_id, name=name, lastName= lastName,gender=gender,age=age,
     smoke=smoke, pregnancy=pregnancy, ethiopian=ethiopian, eastCommunity=eastCommunity,bloodTest=bloodTest)
     patient.save()
+    if (age!= None):
+        diagnose(bloodTest,patient,int(age))
     return render(request,'patientQ.html', {'doctor':doctor,'patient':patient})
-
-
 
 
 
 def addPatientSucc(request,user_id):
     doctor = Doctor.objects.get(user_id = user_id)
     return render(request,'sucsees.html',{'doctor' :doctor})
+
+def diagnose(bloodtest,patient,age):
+    count=0
+    if (patient.medicine == True):
+        count = 1
+    anemia  = ".אנמיה- שני כדורי 10 מג של בי12 ביום למשך חודש"
+    diet = ".דיאטה- לתאם פגישה עם תזונאי"
+    bleading = ".דימום- להתפנות בדחיפות לבית החולים"
+    hiperlipidemia = ".היפרליפידמיה(שומנים בדם)- של לתאם פגישה עם תזונאי, כדור 5 מג של סימוביל ביום למשך שבוע"
+    disruptionOfBlood = ".הפרעה ביצירת הדם\תאי הדם - כדור 10 מג של בי12 ביום למשך חודש וכדור 5 מג של חומצה פולית ביום למשך חודש"
+    hematologicalDisorder = ".הפרעה המטולוגיה- זריקה של הורמון לעידוד ייצור תאי הדם האדומים"
+    ironPoisoning= ".הרעלת ברזל - להתפנות לבית החולים"
+    dehydration = ".התייבשות- מנוחה מוחלטת בשכיבה, החזרת נוזלים בשתייה"
+    infection = ".זיהום- אנטיביוטיקה ייעודית"
+    vitaminDef = ".חוסר בוויטמינם- הפנייה לבדיקת דם לזיהוי הוויטמינים החסרים"
+    viralDisease = ".מחלה ויראלית- לנוח בבית"
+    diseaseInBile = ".מחלות בדרכי המרה- הפנייה לטיפול כירורגי"
+    heartDisease = ".מחלות לב - לתאם פגישה עם תזונאי"
+    bloodDisease = ".מחלת דם- שילוב של ציקלופוספאמיד וקורטיקוסרואידים"
+    liverDisease = ".מחלת כבד- הפנייה לאבחנה ספציפית לצורך קביעת טיפול"
+    kidneyDisease = ".מחלת כליה - איזון את רמות הסוכר בדם"
+    ironDef = ".מחסור בברזל - שני כדורי 10 מג של בי12 ביום למשך חודש"
+    muscleDisease = ".מחלות שריר- שני כדורי 5 מג של כורכום סי3 של אלטמן ביום למשך חודש"
+    smokeing = ".מעשנים- להפסיק לעשן"
+    lungDisease = ".מחלת ריאות- להפסיק לעשן / הפנייה לצילום רנטגן של הריאות"
+    overactiveThyroid = "Propylthiouracil פעילות יתר של בלוטת התריס- להקטנת פעילות בלוטת התריס"
+    adultDiabetes = ".סוכרת מבוגרים- התאמת אינסולין למטופל"
+    cancer = " אנטרקטיניב - Entrectinibסרטן -"
+    meatInc = ".צריכה מוגברת של סרטן- לתאם פגישה עם תזונאי"
+    variousMedications = ".שימוש בתרופות שונות- הפנייה לרופא המשפחה לצורך בדיקת התאמה בין התרופות"
+    malnutrition = ".תת תזונה- לתאם פגישה עם תזונאי"
+
+    #WBC check
+    if (age!=None):
+        if (age>=0 and age<=3):
+            if (int(bloodtest.WBC)<6000):
+                patient.diagnose=json.dumps("ערכים נמוכים של כמות תאי הדם הלבנים הכללית מצביעים על מחלה ויראלית, כשל של מערכת החיסון ובמקרים נדירים ביותר על סרטן.",ensure_ascii=False)
+                patient.treatment=json.dumps(viralDisease,ensure_ascii=False)
+                patient.treatment=json.dumps(cancer,ensure_ascii=False)
+                patient.treatment=json.dumps(bloodDisease,ensure_ascii=False)
+            if (int(bloodtest.WBC)>17500):
+                patient.diagnose=json.dumps(" ערכים גבוהים של כמות תאי הדם הלבנים הכללית מצביעים לרוב על קיומו של זיהום, אם קיימת מחלת חום. במקרים אחרים, נדירים ביותר, עלולים ערכים גבוהים מאוד להעיד על מחלת דם או סרטן. ",ensure_ascii=False)
+                patient.treatment=json.dumps(infection,ensure_ascii=False)
+                patient.treatment=json.dumps(bloodDisease,ensure_ascii=False)
+                patient.treatment=json.dumps(cancer,ensure_ascii=False)
+        elif (age>3 and age<=17):
+            if (int(bloodtest.WBC)<5500):
+                patient.diagnose=json.dumps("ערכים נמוכים של כמות תאי הדם הלבנים הכללית מצביעים על מחלה ויראלית, כשל של מערכת החיסון ובמקרים נדירים ביותר על סרטן.",ensure_ascii=False)
+                patient.treatment=json.dumps(viralDisease,ensure_ascii=False)
+                patient.treatment=json.dumps(cancer,ensure_ascii=False)
+                patient.treatment=json.dumps(bloodDisease,ensure_ascii=False)            
+            if (int(bloodtest.WBC)>15500):
+                patient.diagnose=json.dumps(" ערכים גבוהים של כמות תאי הדם הלבנים הכללית מצביעים לרוב על קיומו של זיהום, אם קיימת מחלת חום. במקרים אחרים, נדירים ביותר, עלולים ערכים גבוהים מאוד להעיד על מחלת דם או סרטן. ",ensure_ascii=False)
+                patient.treatment=json.dumps(infection,ensure_ascii=False)
+                patient.treatment=json.dumps(bloodDisease,ensure_ascii=False)
+                patient.treatment=json.dumps(cancer,ensure_ascii=False)
+        else:
+            if (int(bloodtest.WBC)<4500):
+                patient.diagnose=json.dumps("ערכים נמוכים של כמות תאי הדם הלבנים הכללית מצביעים על מחלה ויראלית, כשל של מערכת החיסון ובמקרים נדירים ביותר על סרטן.",ensure_ascii=False)
+                patient.treatment=json.dumps(viralDisease,ensure_ascii=False)
+                patient.treatment=json.dumps(cancer,ensure_ascii=False)
+                patient.treatment=json.dumps(bloodDisease,ensure_ascii=False)         
+            if (int(bloodtest.WBC)>11000):
+                patient.diagnose=json.dumps(" ערכים גבוהים של כמות תאי הדם הלבנים הכללית מצביעים לרוב על קיומו של זיהום, אם קיימת מחלת חום. במקרים אחרים, נדירים ביותר, עלולים ערכים גבוהים מאוד להעיד על מחלת דם או סרטן. ",ensure_ascii=False)
+                patient.treatment=json.dumps(infection,ensure_ascii=False)
+                patient.treatment=json.dumps(bloodDisease,ensure_ascii=False)
+                patient.treatment=json.dumps(cancer,ensure_ascii=False)
+    #Neut check
+    if(int(bloodtest.Neut)<28):
+        patient.diagnose=json.dumps(" ערכים נמוכים בכמות תאי הדם הלבנים האחראים בעיקר על חיסול החיידקים- מעידים על הפרעה ביצירת הדם, על נטייה לזיהומים מחיידקים ובמקרים נדירים - על תהליך סרטני.",ensure_ascii=False)
+        patient.treatment=json.dumps(disruptionOfBlood,ensure_ascii=False)
+        patient.treatment=json.dumps(cancer,ensure_ascii=False)
+        patient.treatment=json.dumps(infection,ensure_ascii=False)     #כתוב רק נטייה לזיהום         
+        count=count+3
+    if(int(bloodtest.Neut)>54):
+        patient.diagnose=json.dumps("ערכים גבוהים בכמות תאי הדם הלבנים האחראים בעיקר על חיסול החיידקים מעידים על זיהום חיידקי.",ensure_ascii=False)
+        patient.treatment=json.dumps(infection,ensure_ascii=False)        
+        count=count+1
+    #Lymph check
+    if(int(bloodtest.Lymph)<36):
+        patient.diagnose=json.dumps(" ערכים נמוכים תאי דם לבנים האחראים על הריגת נגיפים או חיידקים הנמצאים בגוף זמן ממושך מעידים על בעיה ביצירת תאי דם ",ensure_ascii=False)
+        patient.treatment=json.dumps(disruptionOfBlood,ensure_ascii=False)
+        count=count+2
+    if(int(bloodtest.Lymph)>52):    
+        patient.diagnose=json.dumps("ערכים גבוהים בתאי דם לבנים האחראים על הריגת נגיפים או חיידקים הנמצאים בגוף זמן ממושך עשויים להצביע על זיהום חידקי ממושך או על סרטן הלימפומה",ensure_ascii=False)        
+        patient.treatment=json.dumps(infection,ensure_ascii=False)
+        patient.treatment=json.dumps(cancer,ensure_ascii=False)
+    #RBC check
+    if(int(bloodtest.RBC)<4.5):
+        patient.diagnose=json.dumps(" ערכים נמוכים בכדוריות הדם האדומות אחראיות על קשירת חמצן מהריאות, על הובלתו לרקמות הגוף, על קליטת פחמן דו-חמצנימתאי הגוף השונים ועל פליטתו בחזרה לריאות - עלולים להצביע על אנמיה או על דימומים קשים. ",ensure_ascii=False)
+        patient.treatment=json.dumps(anemia,ensure_ascii=False)
+        patient.treatment=json.dumps(bleading,ensure_ascii=False)
+    if(int(bloodtest.RBC)>6):    
+        patient.diagnose=json.dumps(" ערכים נמוכים בכדוריות הדם האדומות אחראיות על קשירת חמצן מהריאות, על הובלתו לרקמות הגוף, על קליטת פחמן דו-חמצנימתאי הגוף השונים ועל פליטתו בחזרה לריאות -  עלולים להצביע על הפרעה במערכת ייצור הדם. רמות גבוהות נצפו גם אצל מעשנים ואצל חולים במחלות ריאות.",ensure_ascii=False)
+        patient.treatment=json.dumps(lungDisease,ensure_ascii=False)
+        patient.treatment=json.dumps(disruptionOfBlood,ensure_ascii=False)
+        if (patient.smoke == True):
+            patient.treatment=json.dumps(smokeing,ensure_ascii=False)
+    #HCT check
+    if(patient.gender == "woman"):
+        if (int(bloodtest.HCT)<33):
+            patient.diagnose=json.dumps(" ערכים נמוכים בנפח כדוריות הדם האדומות בתוך כלל נוזל הדם. - מצביעים לרוב על דימום או על אנמיה ",ensure_ascii=False)
+            patient.treatment=json.dumps(anemia,ensure_ascii=False)
+            patient.treatment=json.dumps(bleading,ensure_ascii=False)
+        if (int(bloodtest.HCT)>47):
+            patient.diagnose=json.dumps(" ערכים נמוכים בנפח כדוריות הדם האדומות בתוך כלל נוזל הדם. - שכיח בדרך כלל אצל מעשנים.  ",ensure_ascii=False)
+        if (patient.smoke == True):
+            patient.treatment=json.dumps(smokeing,ensure_ascii=False)
+    if(patient.gender == "man"):
+        if (int(bloodtest.HCT)<37):
+            patient.diagnose=json.dumps(" ערכים נמוכים בנפח כדוריות הדם האדומות בתוך כלל נוזל הדם. - מצביעים לרוב על דימום או על אנמיה ",ensure_ascii=False)
+            patient.treatment=json.dumps(anemia,ensure_ascii=False)
+            patient.treatment=json.dumps(bleading,ensure_ascii=False)
+        if (int(bloodtest.HCT)>54):
+            patient.diagnose=json.dumps(" ערכים נמוכים בנפח כדוריות הדם האדומות בתוך כלל נוזל הדם. - שכיח בדרך כלל אצל מעשנים.  ",ensure_ascii=False)
+        if (patient.smoke == True):
+            patient.treatment=json.dumps(smokeing,ensure_ascii=False)
+
+    #Urea check
+    if((patient.eastCommunity == False and int(bloodtest.Urea)<17) or (patient.eastCommunity == True and int(bloodtest.Urea)<18.7)):
+        patient.diagnose=json.dumps("ערכים נמוכים ברמת השתנן בדם. שתנן הוא התוצר הסופי של תהליך חילוף החומרים של חלבונים בגוף- עלול להצביע על: תת תזונה, דיאטה דלת חלבון או מחלת כבד.",ensure_ascii=False)
+        if(patient.pregancy == True):
+            patient.diagnose=json.dumps("המטופלת בהריון - נא לשים לב בהריון רמת השתנן יורדת.",ensure_ascii=False)
+        patient.treatment=json.dumps(malnutrition,ensure_ascii=False)
+        patient.treatment=json.dumps(diet,ensure_ascii=False)
+        patient.treatment=json.dumps(liverDisease,ensure_ascii=False)
+    if((patient.eastCommunity == False and int(bloodtest.Urea)>43) or (patient.eastCommunity == True and int(bloodtest.Urea)>47.3)):
+        patient.diagnose=json.dumps(" ערכים גבוהים ברמת השתנן בדם. שתנן הוא התוצר הסופי של תהליך חילוף החומרים של חלבונים בגוף - עלולים להצביע על מחלות כליה,התייבשות או דיאטה עתירת חלבונים",ensure_ascii=False)
+        patient.treatment=json.dumps(kidneyDisease,ensure_ascii=False)
+        patient.treatment=json.dumps(dehydration,ensure_ascii=False)
+        patient.treatment=json.dumps(diet,ensure_ascii=False)
+
+
+    #HB check
+    if((patient.gender == "woman" and age >17 and int(bloodtest.Hb)<12) or (patient.gender == "man" and age >17 and int(bloodtest.Hb)<12) or (age <=17 and int(bloodtest.Hb)<11.5)):
+        patient.diagnose=json.dumps("ערכים נמוכים ברמת ההמוגלובין- המוגלובין הוא מרכיב בתוך הכדורית האדומה, אשר אחראי על קשירתם ועל שחרורם של חמצן ושל פחמן דו-חמצני-מעידים על אנמיה. זו יכולה לנבוע מהפרעה המטולוגית, ממחסור בברזל ומדימומים.",ensure_ascii=False)
+        patient.treatment=json.dumps(anemia,ensure_ascii=False)
+        patient.treatment=json.dumps(hematologicalDisorder,ensure_ascii=False)
+        patient.treatment=json.dumps(ironDef,ensure_ascii=False)
+        patient.treatment=json.dumps(bleading,ensure_ascii=False)
+
+    if((patient.gender == "woman" and age >17 and int(bloodtest.Hb)>16) or (patient.gender == "man" and age >17 and int(bloodtest.Hb)>18) or (age <=17 and int(bloodtest.Hb)>15.5)):
+        patient.diagnose=json.dumps("ערכים גבוהים ברמת ההמוגלובין- המוגלובין הוא מרכיב בתוך הכדורית האדומה, אשר אחראי על קשירתם ועל שחרורם של חמצן ושל פחמן דו-חמצני.",ensure_ascii=False)
+
+    #Crtn check
+    if((age>=0 and age<=2 and int(bloodtest.Crtn)<0.2) or (age>=3 and age<=17 and int(bloodtest.Crtn)<0.5) or (age>=18 and age<=59 and int(bloodtest.Crtn)<0.6) or (age>=60 and int(bloodtest.Crtn)<0.6)):
+        patient.diagnose=json.dumps("ערכים נמוכים בקריטאינין -  תוצר פירוק של מרכיב המיוצר בגוף ונמצא בשריר הקרוי קריאנין פוספט. בדיקת קריאטינין חשובה ביותר כיוון שהיא נותנת אמת מידה לגבי תפקוד הכליות. ערכים נמוכים נראים לרוב בחולים בעלי מסת שריר ירודה מאוד ואנשים בתת תזונה שאינם צורכים די חלבון",ensure_ascii=False)
+        patient.treatment=json.dumps(muscleDisease,ensure_ascii=False)
+        patient.treatment=json.dumps(malnutrition,ensure_ascii=False)
+    if((age>=0 and age<=2 and int(bloodtest.Crtn)>0.5) or (age>=3 and age<=17 and int(bloodtest.Crtn)>1) or (age>=18 and age<=59 and int(bloodtest.Crtn)>1) or (age>=60 and int(bloodtest.Crtn)>1.2)):
+        patient.diagnose=json.dumps("ערכים גבוהים בקריטאינין - תוצר פירוק של מרכיב המיוצר בגוף ונמצא בשריר הקרוי קריאנין פוספט. בדיקת קריאטינין חשובה ביותר כיוון שהיא נותנת אמת מידה לגבי תפקוד הכליות- הערכים עלולים להצביע על בעיה כלייתית ובמקרים חמורים על אי ספיקת כליות. ערכים גבוהים ניתן למצוא גם בעת שלשולים והקאות (הגורמים לפירוק מוגבר של שריר ולערכים גבוהים של קריאטינין), מחלות שריר וצריכה מוגברת של בשר.",ensure_ascii=False)
+        patient.treatment=json.dumps(kidneyDisease,ensure_ascii=False)
+        patient.treatment=json.dumps(muscleDisease,ensure_ascii=False)
+        patient.treatment=json.dumps(meatInc,ensure_ascii=False)
+
+    #Iron check
+    if ((patient.gender == "man" and int(bloodtest.Iron)<60) or (patient.gender == "woman" and int(bloodtest.Iron)<48)):
+        patient.diagnose=json.dumps("ערכים נמוכים בברזל - הברזל חיוני ליצירת ההמוגלובין - החלבון שנושא את החמצן בדם. נוסף על כך הוא משמש ליצירת אנזימים רבים אחרים. רמות נמוכות של ברזל מעידה בדרך כלל על תזונה לא מספקת או על עלייה בצורך בברזל (למשל בהריון) או על איבוד דם בעקבות דימום.",ensure_ascii=False)
+        patient.treatment=json.dumps(diet,ensure_ascii=False)
+        patient.treatment=json.dumps(bleading,ensure_ascii=False)
+        #checkkkkkkkkkkkkk
+    if ((patient.gender == "man" and int(bloodtest.Iron)>160) or (patient.gender == "woman" and int(bloodtest.Iron)>128)):
+        patient.diagnose=json.dumps("ערכים גבוהים בברזל - הברזל חיוני ליצירת ההמוגלובין - החלבון שנושא את החמצן בדם. נוסף על כך הוא משמש ליצירת אנזימים רבים אחרים. רמות גבוהות של עלולים להצביע על הרעלת ברזל",ensure_ascii=False)
+        patient.treatment=json.dumps(ironPoisoning,ensure_ascii=False)
+
+    #HDL check
+    if ((patient.ethiopian == False and (patient.gender == "man" and int(bloodtest.HDL)<29) or (patient.gender == "woman" and int(bloodtest.HDL)<34)) or (patient.ethiopian == True and (patient.gender == "man" and int(bloodtest.HDL)<34.8) or (patient.gender == "woman" and int(bloodtest.HDL)<40.8)) ):
+        patient.diagnose=json.dumps("ערכים נמוכים באץ'-די-אל הקרוי גם הכולסטרול הטוב, הינו מולקולה דמוית חלבון, אשר נושאת את הכולסטרול מתאי הגוף אל הכבד,שם מפורק הכולסטרול. בכך מסייע ה-אץ'-די-אל לגוף להיפטר מעודפי שומנים. רמות נמוכותת עשויות להצביע על סיכון למחלות לב, על היפרליפידמיה (יתר שומנים בדם) או על סוכרת מבוגרים.",ensure_ascii=False)
+        patient.treatment=json.dumps(heartDisease,ensure_ascii=False)
+        patient.treatment=json.dumps(hiperlipidemia,ensure_ascii=False)
+        patient.treatment=json.dumps(adultDiabetes,ensure_ascii=False)
+    if ((patient.ethiopian == False and (patient.gender == "man" and int(bloodtest.HDL)>62) or (patient.gender == "woman" and int(bloodtest.HDL)>82)) or (patient.ethiopian == True and (patient.gender == "man" and int(bloodtest.HDL)>62*1.2) or (patient.gender == "woman" and int(bloodtest.HDL)>82*1.2)) ):
+        patient.diagnose=json.dumps("ערכים גבוהים באץ'-די-אל הקרוי גם הכולסטרול הטוב, הינו מולקולה דמוית חלבון, אשר נושאת את הכולסטרול מתאי הגוף אל הכבד,שם מפורק הכולסטרול. בכך מסייע ה-אץ'-די-אל לגוף להיפטר מעודפי שומנים. רמות גבוהות לרוב אינן מזיקות. פעילות גופנית מעלה את רמות הכולסטרול הטוב.",ensure_ascii=False)
+
+    #AP check
+    if ((patient.eastCommunity == False and  int(bloodtest.AP)<30) or (patient.eastCommunity == True and  int(bloodtest.AP)<60)):
+        patient.diagnose=json.dumps("ערכים נמוכים בפוספטזה אלקלית - התפקיד המטבולי של האנזים אינו ברור לגמרי. הוא קשור להעברת מרכיבים דרך ממברנות, וכן יש לו תפקיד בהסתיידות של העצם. האנזים נמצא ברקמות שונות בגוף, בעיקר בכבד, בדרכי המרה, במעי, בעצמות ובשליה. רמות נמוכות עשויות להצביע על תזונה לקויה שחסרים בה חלבונים. חוסר בוויטמינים כמו ויטמין בי12 , ויטמין סי, ויטמין בי6 וחומצה פולית. ",ensure_ascii=False)
+        patient.treatment=json.dumps(diet,ensure_ascii=False)
+        patient.treatment=json.dumps(vitaminDef,ensure_ascii=False)
+    if ((patient.eastCommunity == False and  int(bloodtest.AP)>90) or (patient.eastCommunity == True and  int(bloodtest.AP)>120)):
+        patient.diagnose=json.dumps("ערכים גבוהים  בפוספטזה אלקלית - התפקיד המטבולי של האנזים אינו ברור לגמרי. הוא קשור להעברת מרכיבים דרך ממברנות, וכן יש לו תפקיד בהסתיידות של העצם. האנזים נמצא ברקמות שונות בגוף, בעיקר בכבד, בדרכי המרה, במעי, בעצמות ובשליה. רמות גבוהות עלולים להצביע על מחלות כבד, מחלות בדרכי המרה, הריון, פעילות יתר של בלוטת התריס או שימוש בתרופות שונות",ensure_ascii=False)
+        patient.treatment=json.dumps(liverDisease,ensure_ascii=False)
+        patient.treatment=json.dumps(diseaseInBile,ensure_ascii=False)
+        patient.treatment=json.dumps(overactiveThyroid,ensure_ascii=False)
+        patient.treatment=json.dumps(variousMedications,ensure_ascii=False)
+        if (patient.pregnancy == True):
+            patient.treatment=json.dumps("המטופלת בהריון - נא לשים לב יכולות להיות רמות גבוהות בגלל ההריון",ensure_ascii=False)
+        if (patient.pregnancy == False and patient.gender == "woman"):
+            patient.treatment=json.dumps("עלול להיגרם בגלל הריון - הפנייה לשלילת הריון",ensure_ascii=False)
+
